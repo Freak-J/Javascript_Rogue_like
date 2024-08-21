6ef8a44fd1177eb6668b4f1cac9c78f1ad937f53
전지훈 <jihun1162@gmail.com>
2024-08-21 20:28:28 +0900

    v1.004

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ import readlineSync from 'readline-sync';
 
 class Player {
   constructor() {
-    this.hp = 100;
+    this.hp = 1000;
+    this.Attack = 100;
   }
 
   attack() {
@@ -12,8 +13,9 @@ class Player {
 }
 
 class Monster {
-  constructor() {
-    this.hp = 100;
+  constructor(stage) {
+    this.hp = 100 + (stage*10);
+    this.Attack = 3 + (stage*2);
   }
 
   attack() {
@@ -26,10 +28,10 @@ function displayStatus(stage, player, monster) {
   console.log(
     chalk.cyanBright(`| Stage: ${stage} `) +
     chalk.blueBright(
-      `| 플레이어 정보`,
+      `| 플레이어 정보 HP: ${player.hp} Attack: ${player.Attack} `,
     ) +
     chalk.redBright(
-      `| 몬스터 정보 |`,
+      `| 몬스터 정보 HP: ${monster.hp} Attack: ${monster.Attack} `,
     ),
   );
   console.log(chalk.magentaBright(`=====================\n`));
@@ -37,8 +39,15 @@ function displayStatus(stage, player, monster) {
 
 const battle = async (stage, player, monster) => {
   let logs = [];
+  let addHp = 10;
+  let count = 1;
 
-  while(player.hp > 0) {
+  if (stage >= 2) {
+    logs.push(chalk.greenBright(`체력이 ${addHp} 회복되었습니다.`))
+    player.hp = player.hp + addHp;
+  }
+
+  while (player.hp > 0) {
     console.clear();
     displayStatus(stage, player, monster);
 
@@ -46,15 +55,28 @@ const battle = async (stage, player, monster) => {
 
     console.log(
       chalk.green(
-        `\n1. 공격한다 2. 아무것도 하지않는다.`,
+        `\n1. 공격한다 2. 도망치기.`,
       ),
     );
     const choice = readlineSync.question('당신의 선택은? ');
 
     // 플레이어의 선택에 따라 다음 행동 처리
-    logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
+    switch (choice) {
+      case '1':
+        logs.push(chalk.green(`[${count}] 몬스터에게 ${player.Attack}의 피해를 입혔습니다.`));
+        monster.hp = monster.hp - player.Attack;
+        logs.push(chalk.red(`[${count}] 몬스터가 ${monster.Attack}의 피해를 입혔습니다.`));
+        player.hp = player.hp - monster.Attack;
+        break;
+      case '2':
+        return 0;
+    }
+    if(monster.hp <= 0) {
+      return 0;
+    }
+    count++
   }
-  
+
 };
 
 export async function startGame() {
@@ -65,9 +87,27 @@ export async function startGame() {
   while (stage <= 10) {
     const monster = new Monster(stage);
     await battle(stage, player, monster);
-
+    if(player.hp <= 0) {
+      console.log(chalk.redBright(`패배하셨습니다!`));
+      const choice = readlineSync.question('다시시작하시겠습니까? 1 : 예, 2: 아니오');
+      switch(choice){
+        case '1':
+          startGame();
+        case '2':
+          return 0;
+      }
+    }
     // 스테이지 클리어 및 게임 종료 조건
-
     stage++;
+    if(monster.hp <= 0 && stage > 10){
+      console.log(chalk.greenBright(`Stage를 모두 클리어하셨습니다!`));
+      const choice = readlineSync.question('다시시작하시겠습니까? 1 : 예, 2: 아니오');
+      switch(choice){
+        case '1':
+          startGame();
+        case '2':
+          return 0;
+      }
+    }
   }
 }
